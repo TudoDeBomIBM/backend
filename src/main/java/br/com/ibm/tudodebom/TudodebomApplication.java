063f92466a9e6b94ecbc9ab221a5799bc9ece818
Jessica Santos <jm.jeska@gmail.com>
2022-08-13 15:01:28 -0300

    Criando projeto inicial

---
+++ src/main/java/br/com/ibm/tudodebom/TudodebomApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibm.tudodebom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudodebomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TudodebomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ibm/tudodebom/TudodebomApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ibm.tudodebom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TudodebomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
