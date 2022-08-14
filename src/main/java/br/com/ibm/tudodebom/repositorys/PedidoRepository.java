99cb5e793802f7525cc73fc1aa934e18ca26d6ee
Jessica Santos <jm.jeska@gmail.com>
2022-08-14 00:29:36 -0300

    Adicionando pedidoRepository

---
+++ src/main/java/br/com/ibm/tudodebom/repositorys/PedidoRepository.java
@@ -0,0 +1,9 @@
+package br.com.ibm.tudodebom.repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<PedidoEntity, Long>{
+
+}
