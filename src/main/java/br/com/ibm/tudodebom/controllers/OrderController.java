2e1f6f0aee8619cb22d787c0e1f66bcd53118022
Welton Vitor <wvitorsj08@gmail.com>
2022-08-17 15:23:11 -0300

    Commit Final

--- src/main/java/br/com/ibm/tudodebom/controllers/OrderController.java
+++ src/main/java/br/com/ibm/tudodebom/controllers/OrderController.java
@@ -2,19 +2,14 @@ package br.com.ibm.tudodebom.controllers;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import br.com.ibm.tudodebom.dtos.requests.RequestOrderDTO;
 import br.com.ibm.tudodebom.dtos.responses.ResponseOrderDTO;
 import br.com.ibm.tudodebom.services.OrderService;
 
@@ -37,13 +32,13 @@ public class OrderController {
         return ResponseEntity.ok(responseOrderDTO);
     }
     
-    @PostMapping
-	public ResponseEntity<?> post(@Valid @RequestBody RequestOrderDTO requestOrderDTO, UriComponentsBuilder uriComponentsBuilder) {
-		var responseOrderDTO = orderService.save(requestOrderDTO);
-		//URI uri = uriComponentsBuilder.path("/{idProduct}/orders").buildAndExpand(responseOrderDTO.getId()).toUri();
-		//return ResponseEntity.created(uri).body(responseOrderDTO);
-		return ResponseEntity.ok(responseOrderDTO);
+    @DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		orderService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
+    
+    
 
     /*@PutMapping("/{idProduct}/orders")
     public ResponseEntity<Void> update(@RequestBody @Valid RequestOrderDTO request, @PathVariable Long idProduct) {
--- src/main/java/br/com/ibm/tudodebom/dtos/responses/ResponseOrderDetails.java
+++ src/main/java/br/com/ibm/tudodebom/dtos/responses/ResponseOrderDetails.java
@@ -2,11 +2,14 @@ package br.com.ibm.tudodebom.dtos.responses;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import lombok.Data;
 
 @Data
 public class ResponseOrderDetails {
 	private Long id;
+	@JsonBackReference
 	private ResponseOrderDTO order;
 	private ResponseProductDTO product;
 	private BigDecimal unityPrice;
