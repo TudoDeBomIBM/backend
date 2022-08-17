345a9107b9bceace0301a8a7ed416d220063bdf8
Jessica Santos <jm.jeska@gmail.com>
2022-08-17 17:32:45 -0300

    Readicionando método post na orderController que foi excluido indevidamente

--- src/main/java/br/com/ibm/tudodebom/controllers/OrderController.java
+++ src/main/java/br/com/ibm/tudodebom/controllers/OrderController.java
@@ -1,15 +1,22 @@
 package br.com.ibm.tudodebom.controllers;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.ibm.tudodebom.dtos.requests.RequestOrderDTO;
 import br.com.ibm.tudodebom.dtos.responses.ResponseOrderDTO;
 import br.com.ibm.tudodebom.services.OrderService;
 
@@ -38,8 +45,13 @@ public class OrderController {
 		return ResponseEntity.noContent().build();
 	}
     
+    @PostMapping
+    public ResponseEntity<?> post(@Valid @RequestBody RequestOrderDTO requestOrderDTO, UriComponentsBuilder uriComponentsBuilder) {
+        Long insertedId = orderService.save(requestOrderDTO);
+        URI uri = uriComponentsBuilder.path("/orders/{id}").buildAndExpand(insertedId).toUri();
+        return ResponseEntity.created(uri).body(null);
+    }
     
-
     /*@PutMapping("/{idProduct}/orders")
     public ResponseEntity<Void> update(@RequestBody @Valid RequestOrderDTO request, @PathVariable Long idProduct) {
         orderService.update(request, idProduct);
